5c662fdb69624a064d8fe0e4a4cf87c630caee60
Tony <tony.xing.91@gmail.com>
2018-05-22 11:02:45 +0200

    reviewed code

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   devise_for :vans
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+   # get "pages/profile", to: "pages#profile"
 end
