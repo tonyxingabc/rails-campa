ccb4082dfab62631a3b6968302edc18f997c3abe
Tony <tony.xing.91@gmail.com>
2018-05-24 10:19:51 +0200

    pre-demo-wednesday

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,9 @@ User.destroy_all
 Van.destroy_all
 Booking.destroy_all
 
-u1 = User.create({email: "Lito@gmail.com", password:"1111111", name: "Lito", username: "Litotechie"})
-u2 = User.create({email: "Tony@gmail.com", password:"1111111",name: "Tony", username: "tonyxingabc"})
-u3 = User.create({email: "Ferdi@gmail.com", password:"1111111",name: "Ferdi", username: "ferdi"})
+u1 = User.create({email: "lito@gmail.com", password:"1111111", name: "Lito", username: "Litotechie"})
+u2 = User.create({email: "tony@gmail.com", password:"1111111",name: "Tony", username: "tonyxingabc"})
+u3 = User.create({email: "ferdi@gmail.com", password:"1111111",name: "Ferdi", username: "ferdi"})
 
 v1 = Van.create({user_id: u1.id, description: "The most beautiful van in the world", cost_per_night: 70, max_guest: 2, transmission_type: "manual", toilet: true, beds: 2, location: "Berlin"})
 v2 = Van.create({user_id: u2.id, description: "The most ugly van in the world", cost_per_night: 20, max_guest: 2, transmission_type: "manual", toilet: false, beds: 2, location: "Auckland"})
