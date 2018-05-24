37592b1a14ebdd5e99a98f56381c4dd9bef06115
Emanuel Coen <emanuelcoen@gmail.com>
2018-05-24 19:13:18 +0200

    Some profile changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,9 +14,9 @@ u1 = User.create({email: "lito@gmail.com", password:"1111111", name: "Lito", use
 u2 = User.create({email: "tony@gmail.com", password:"1111111",name: "Tony", username: "tonyxingabc"})
 u3 = User.create({email: "ferdi@gmail.com", password:"1111111",name: "Ferdi", username: "ferdi"})
 
-v1 = Van.create({user_id: u1.id, description: "The most beautiful van in the world", cost_per_night: 70, max_guest: 2, transmission_type: "manual", toilet: true, beds: 2, location: "Berlin"})
-v2 = Van.create({user_id: u2.id, description: "The most ugly van in the world", cost_per_night: 20, max_guest: 2, transmission_type: "manual", toilet: false, beds: 2, location: "Auckland"})
-v3 = Van.create({user_id: u2.id, description: "The most ugly van in the world", cost_per_night: 20, max_guest: 2, transmission_type: "manual", toilet: false, beds: 2, location: "Auckland", address: "Implerstraße 12a, Munich"})
-v4 = Van.create({user_id: u1.id, description: "The most ugly van in the world", cost_per_night: 20, max_guest: 2, transmission_type: "manual", toilet: false, beds: 2, location: "Auckland", address: "16 Villa Gaudelet, Paris"})
+v1 = Van.create({user_id: u1.id, description: "The most beautiful van in the world", cost_per_night: 70, max_guest: 2, transmission_type: "manual", toilet: true, beds: 2, location: "Berlin", address: "Avenue Kamerdelle 28, Brüssel", photo: cl_image_tag("seed.jpg")})
+v2 = Van.create({user_id: u2.id, description: "The most ugly van in the world", cost_per_night: 20, max_guest: 2, transmission_type: "manual", toilet: false, beds: 2, location: "Auckland", address: "Avenue Kamerdelle 28, Brüssel", photo: cl_image_tag("seed.jpg")})
+v3 = Van.create({user_id: u2.id, description: "The most ugly van in the world", cost_per_night: 20, max_guest: 2, transmission_type: "manual", toilet: false, beds: 2, location: "Auckland", address: "Implerstraße 12a, Munich", photo: cl_image_tag("seed.jpg")})
+v4 = Van.create({user_id: u1.id, description: "The most ugly van in the world", cost_per_night: 20, max_guest: 2, transmission_type: "manual", toilet: false, beds: 2, location: "Auckland", address: "16 Villa Gaudelet, Paris", photo: cl_image_tag("seed.jpg")})
 
 puts "finished"
