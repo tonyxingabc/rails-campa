7640ba6c993f14e0ef3181754f43e112ac4d2bcb
Tony <tony.xing.91@gmail.com>
2018-05-25 14:44:22 +0200

    add images to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
 Booking.destroy_all
 Van.destroy_all
 User.destroy_all
-
+#van pictures
 url1 = "http://res.cloudinary.com/dgccrihdr/image/upload/v1527184918/jdgr1sy8ipntnherc9cc.jpg"
 url2 = "http://res.cloudinary.com/dgccrihdr/image/upload/v1527238281/fcuugizhjwtz7mqwp5h6.jpg"
+#profile pictures
+url3 = "http://res.cloudinary.com/dgccrihdr/image/upload/v1527186297/zlkezooorwzs6mzuwopt.jpg"
 
-u1 = User.create({email: "lito@gmail.com", password:"1111111", name: "Lito", username: "Litotechie"})
+u1 = User.new(
+  {email: "lito@gmail.com", password:"1111111", name: "Lito", username: "Litotechie"}
+  )
+u1.remote_photo_url = url3
+u1.save
 u2 = User.create({email: "tony@gmail.com", password:"1111111",name: "Tony", username: "tonyxingabc"})
 u3 = User.create({email: "ferdi@gmail.com", password:"1111111",name: "Ferdi", username: "ferdi"})
-
 
 v1 = Van.new(
   {user_id: u1.id, title: "Van 1", description: "The most beautiful van in the world", cost_per_night: 70, max_guest: 2, transmission_type: "manual", toilet: true, beds: 2, location: "Berlin", address: "Avenue Kamerdelle 28, Brüssel"}
@@ -30,7 +42,6 @@ v2.save
 v3 = Van.create({user_id: u2.id, title: "Van 3", description: "The most ugly van in the world", cost_per_night: 20, max_guest: 2, transmission_type: "manual", toilet: false, beds: 2, location: "Auckland", address: "Implerstraße 12a, Munich"})
 v3.remote_photo_url = url1
 v3.save
-
 
 v4 = Van.new({user_id: u1.id, title: "Van 4", description: "The most ugly van in the world", cost_per_night: 20, max_guest: 2, transmission_type: "manual", toilet: false, beds: 2, location: "Auckland", address: "16 Villa Gaudelet, Paris"})
 v4.remote_photo_url = url2
